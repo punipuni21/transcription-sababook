d24b993d281445abcb92842cb74cee45ab39893b
punipuni21 <watanaberyotaroyp@gmail.com>
2025-04-10 23:18:18 +0900

    getter

--- ch2/saba/saba_core/src/url.rs
+++ ch2/saba/saba_core/src/url.rs
@@ -11,6 +11,19 @@ pub struct Url {
 }
 
 impl Url {
+    pub fn host(&self) -> String {
+        self.host.clone()
+    }
+    pub fn port(&self) -> String {
+        self.port.clone()
+    }
+    pub fn path(&self) -> String {
+        self.path.clone()
+    }
+    pub fn searchpart(&self) -> String {
+        self.searchpart.clone()
+    }
+
     pub fn new(url: String) -> Self {
         Self {
             url: "".to_string(),
