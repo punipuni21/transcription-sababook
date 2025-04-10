8f795844d352cb5e971fb95a42ecab1499658c5c
punipuni21 <watanaberyotaroyp@gmail.com>
2025-04-10 23:30:07 +0900

    test

--- ch2/saba/saba_core/src/url.rs
+++ ch2/saba/saba_core/src/url.rs
@@ -113,3 +113,21 @@ impl Url {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_url_host() {
+        let url = "http://example.com".to_string();
+        let expected = Ok(Url {
+            url: url.clone(),
+            host: "example.com".to_string(),
+            port: "80".to_string(),
+            path: "".to_string(),
+            searchpart: "".to_string(),
+        });
+        assert_eq!(expected, Url::new(url).parse());
+    }
+}
