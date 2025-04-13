6572b659e153be421a1ba2f2039fde2c30a53b50
punipuni21 <watanaberyotaroyp@gmail.com>
2025-04-13 20:47:12 +0900

    ip address resolition

--- ch3/saba/net/wasabi/src/http.rs
+++ ch3/saba/net/wasabi/src/http.rs
@@ -1,7 +1,10 @@
 extern crate alloc;
 
+use alloc::format;
 use alloc::string::String;
+use alloc::string::ToString;
 use noli::net::lookup_host;
+use noli::net::SocketAddr;
 use saba_core::error::Error;
 use saba_core::http::HttpResponse;
 
@@ -13,9 +16,18 @@ impl HttpClient {
     }
 
     pub fn get(&self, host: String, port: u16, path: String) -> Result<HttpResponse, Error> {
-      let ips = match lookup_host(&"example.com") {
-        Ok(ips) => ips,
-        Err(_) => return Err(Error::Network("Failed to find IP address").to_string());
-      }
+        let ips = match lookup_host(&"example.com") {
+            Ok(ips) => ips,
+            Err(e) => {
+                return Err(Error::Network(format!(
+                    "Failed to find IP address: {:#?}",
+                    e
+                )))
+            }
+        };
+
+        if ips.len() < 1 {
+            return Err(Error::Network("No IP address found".to_string()));
+        }
     }
 }
