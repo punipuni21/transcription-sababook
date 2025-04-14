3a13442d2c41874a6eb5ee4d533f6b925170b390
punipuni21 <watanaberyotaroyp@gmail.com>
2025-04-14 23:08:23 +0900

    fix main

--- ch3/saba/src/main.rs
+++ ch3/saba/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    print!("Hello, world!");
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+
+use crate::alloc::string::ToString;
+use net_wasabi::http::HttpClient;
+use noli::prelude::*;
+
+fn main() -> u64 {
+    let client = HttpClient::new();
+    match client.get("example.com".to_string(), 80, "".to_string()) {
+        Ok(res) => {
+            print!("response:\n{:#?}", res);
+        }
+        Err(e) => {
+            print!("error:\n{:#?}", e);
+        }
+    }
+    0
 }
+
+entry_point!(main);
