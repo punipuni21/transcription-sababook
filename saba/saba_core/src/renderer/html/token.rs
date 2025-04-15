298612f5215a4f631ac17d3c4565bc0ad15d619e
punipuni21 <watanaberyotaroyp@gmail.com>
2025-04-15 22:44:33 +0900

    HtmlTokenizer

---
+++ saba/saba_core/src/renderer/html/token.rs
@@ -0,0 +1,12 @@
+use alloc::string::String;
+use alloc::vec::Vec;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct HtmlTokenizer {
+    state: State,
+    pos: usize,
+    reconsume: bool,
+    latest_token: Option<HtmlToken>,
+    input: Vec<char>,
+    buf: String,
+}
