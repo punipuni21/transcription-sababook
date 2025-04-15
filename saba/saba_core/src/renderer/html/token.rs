d612d9254e9b1ccbe57480ae69b4395fbb62e288
punipuni21 <watanaberyotaroyp@gmail.com>
2025-04-15 23:51:20 +0900

    implement Iterator

--- saba/saba_core/src/renderer/html/token.rs
+++ saba/saba_core/src/renderer/html/token.rs
@@ -3,29 +3,6 @@ use alloc::string::String;
 use alloc::vec::Vec;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct HtmlTokenizer {
-    state: State,
-    pos: usize,
-    reconsume: bool,
-    latest_token: Option<HtmlToken>,
-    input: Vec<char>,
-    buf: String,
-}
-
-impl HtmlTokenizer {
-    pub fn new(html: String) -> Self {
-        Self {
-            state: State::Data,
-            pos: 0,
-            reconsume: false,
-            latest_token: None,
-            input: html.chars().collect(),
-            buf: String::new(),
-        }
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum HtmlToken {
     StartTag {
         tag: String,
@@ -77,4 +54,43 @@ pub enum State {
     ScriptDataEndTagName,
     /// https://html.spec.whatwg.org/multipage/parsing.html#temporary-buffer
     TemporaryBuffer,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct HtmlTokenizer {
+    state: State,
+    pos: usize,
+    reconsume: bool,
+    latest_token: Option<HtmlToken>,
+    input: Vec<char>,
+    buf: String,
+}
+
+impl HtmlTokenizer {
+    pub fn new(html: String) -> Self {
+        Self {
+            state: State::Data,
+            pos: 0,
+            reconsume: false,
+            latest_token: None,
+            input: html.chars().collect(),
+            buf: String::new(),
+        }
+    }
+}
+
+impl Iterator for HtmlTokenizer {
+    type Item = HtmlToken;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.pos >= self.input.len() {
+            return None;
+        }
+        loop {
+            match self.state {
+                State::Data => {}
+                _ => {}
+            }
+        }
+    }
 }
