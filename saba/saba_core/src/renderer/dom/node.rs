787a7d3efe82c1b47cb81fc91e8f239eb1eca859
punipuni21 <watanaberyotaroyp@gmail.com>
2025-04-25 23:26:10 +0900

    impl node

--- saba/saba_core/src/renderer/dom/node.rs
+++ saba/saba_core/src/renderer/dom/node.rs
@@ -1,26 +1,39 @@
-use core::cell::RefCell;
-use alloc::rc::Weak;
 use alloc::rc::Rc;
+use alloc::rc::Weak;
 use alloc::string::String;
 use alloc::vec::Vec;
+use core::cell::RefCell;
 
 use crate::renderer::html::attribute::Attribute;
 
-
-#[derive(Debug,Clone)]
-pub struct Node{
-  pub kind: NodeKind,
-  window:Weak<RefCell<Window>>,
-  parent: Weak<RefCell<Node>>,
-  first_child: Option<Rc<RefCell<Node>>,
-  last_child: Weak<RefCell<Node>>,
-  previous_sibling: Weak<RefCell<Node>>,
-  next_sibling: Option<Rc<RefCell<Node>>>,
+#[derive(Debug, Clone)]
+pub struct Node {
+    pub kind: NodeKind,
+    window: Weak<RefCell<Window>>,
+    parent: Weak<RefCell<Node>>,
+    first_child: Option<Rc<RefCell<Node>>>,
+    last_child: Weak<RefCell<Node>>,
+    previous_sibling: Weak<RefCell<Node>>,
+    next_sibling: Option<Rc<RefCell<Node>>>,
 }
 
-#[derive(Debug,Clone)]
-pub struct Window{
-  document: Rc<RefCell<Node>>,
+impl Node {
+    pub fn new(kind: NodeKind) -> Self {
+        Self {
+            kind,
+            window: Weak::new(),
+            parent: Weak::new(),
+            first_child: None,
+            last_child: Weak::new(),
+            previous_sibling: Weak::new(),
+            next_sibling: None,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Window {
+    document: Rc<RefCell<Node>>,
 }
 
 #[derive(Debug, Clone)]
@@ -33,10 +46,10 @@ pub enum NodeKind {
     Text(String),
 }
 
-#[derive(Debug,Clone)]
-pub struct Element{
-  kind: ElementKind,
-  attributes:Vec<Attribute>,
+#[derive(Debug, Clone)]
+pub struct Element {
+    kind: ElementKind,
+    attributes: Vec<Attribute>,
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
