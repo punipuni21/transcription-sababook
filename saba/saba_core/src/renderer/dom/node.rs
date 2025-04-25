248fe5cdb21fe15dea759fb8390ce51fc2a64634
punipuni21 <watanaberyotaroyp@gmail.com>
2025-04-25 23:35:28 +0900

    [add] impl Window

--- saba/saba_core/src/renderer/dom/node.rs
+++ saba/saba_core/src/renderer/dom/node.rs
@@ -2,6 +2,7 @@ use alloc::rc::Rc;
 use alloc::rc::Weak;
 use alloc::string::String;
 use alloc::vec::Vec;
+use core::cell::Ref;
 use core::cell::RefCell;
 
 use crate::renderer::html::attribute::Attribute;
@@ -69,11 +70,34 @@ impl Node {
     pub fn next_sibling(&self) -> Option<Rc<RefCell<Node>>> {
         self.next_sibling.as_ref().cloned()
     }
+
+    pub fn set_window(&mut self, window: Weak<RefCell<Window>>) {
+        self.window = window;
+    }
 }
 
 #[derive(Debug, Clone)]
 pub struct Window {
     document: Rc<RefCell<Node>>,
+}
+
+impl Window {
+    pub fn new() -> Self {
+        let window = Self {
+            document: Rc::new(RefCell::new(Node::new(NodeKind::Document))),
+        };
+
+        window
+            .document
+            .borrow_mut()
+            .set_window(Rc::downgrade(&Rc::new(RefCell::new(window.clone()))));
+
+        window
+    }
+
+    pub fn document(&self) -> Rc<RefCell<Node>> {
+        self.document.clone()
+    }
 }
 
 #[derive(Debug, Clone)]
