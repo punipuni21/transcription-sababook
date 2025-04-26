60e099545b258c83c7084b08ced4b8fdfe2fbaa4
punipuni21 <watanaberyotaroyp@gmail.com>
2025-04-26 22:49:05 +0900

    [add] impl HtmlParser

--- saba/saba_core/src/renderer/html/parser.rs
+++ saba/saba_core/src/renderer/html/parser.rs
@@ -1,5 +1,6 @@
 use crate::renderer::dom::node::Node;
 use crate::renderer::dom::node::NodeKind;
+use crate::renderer::dom::node::Window;
 use crate::renderer::html::token::HtmlTokenizer;
 use alloc::rc::Rc;
 use alloc::vec::Vec;
@@ -14,4 +15,16 @@ pub struct HtmlParser {
     /// https://html.spec.whatwg.org/multipage/parsing.html#the-stack-of-open-elements
     stack_of_open_elements: Vec<Rc<RefCell<Node>>>,
     t: HtmlTokenizer,
+}
+
+impl HtmlParser {
+    pub fn new(t: HtmlTokenizer) -> Self {
+        Self {
+            window: Rc::new(RefCell::new(Window::new())),
+            mode: InsertionMode::Initial,
+            original_insertion_mode: InsertionMode::Initial,
+            stack_of_open_elements: Vec::new(),
+            t,
+        }
+    }
 }
