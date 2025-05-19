19ed9bc5238af7c3cb746cc034caadc9faf91058
punipuni21 <watanaberyotaroyp@gmail.com>
2025-05-19 23:26:30 +0900

    [add] LayoutView

---
+++ saba/saba_core/src/renderer/layout/layout_view.rs
@@ -0,0 +1,31 @@
+use core::cell::RefCell;
+
+use alloc::rc::Rc;
+
+use crate::renderer::{
+    css::cssom::StyleSheet,
+    dom::node::{Element, ElementKind, Node},
+};
+
+#[derive(Debug, Clone)]
+pub struct LayoutView {
+    root: Option<Rc<RefCell<LayoutObject>>>,
+}
+
+impl LayoutView {
+    pub fn new(root: Rc<RefCell<Node>>, cssom: &StyleSheet) -> Self {
+        let body_root = get_target_element_node(Some(root), ElementKind::Body);
+
+        let mut tree = Self {
+            root: build_layout_tree(&body_root, &None, cssom),
+        };
+
+        tree.update_layout();
+
+        tree
+    }
+
+    pub fn root(&self) -> Option<Rc<RefCell<LayoutObject>>> {
+        self.root.clone()
+    }
+}
