e22d571d0102fd97d3f7e2c0940a4b142bc4ae5b
punipuni21 <watanaberyotaroyp@gmail.com>
2025-05-19 22:50:11 +0900

    [add] test_one_rule

--- saba/saba_core/src/renderer/css/cssom.rs
+++ saba/saba_core/src/renderer/css/cssom.rs
@@ -249,6 +249,10 @@ pub type ComponentValue = CssToken;
 #[cfg(test)]
 mod tests {
 
+    use alloc::vec;
+
+    use crate::renderer::css;
+
     use super::*;
 
     #[test]
@@ -258,5 +262,28 @@ mod tests {
         let cssom = CssParser::new(t).parse_stylesheet();
 
         assert_eq!(cssom.rules.len(), 0);
+    }
+
+    #[test]
+    fn test_one_rule() {
+        let style = "p { color: red; }".to_string();
+        let t = CssTokenizer::new(style);
+        let cssom = CssParser::new(t).parse_stylesheet();
+
+        let mut rule = QualifiedRule::new();
+        rule.set_selector(Selector::TypeSelector("p".to_string()));
+        let mut declaration = Declaration::new();
+        declaration.set_property("color".to_string());
+        declaration.set_value(ComponentValue::Ident("red".to_string()));
+        rule.set_declarations(vec![declaration]);
+
+        let expected = vec![rule];
+        assert_eq!(cssom.rules.len(), expected.len());
+
+        let mut i = 0;
+        for rule in &cssom.rules {
+            assert_eq!(&expected[i], rule);
+            i += 1;
+        }
     }
 }
