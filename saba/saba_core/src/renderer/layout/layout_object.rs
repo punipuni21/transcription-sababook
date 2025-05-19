7b1b6d7922938e45a106405724efcb48a521e262
punipuni21 <watanaberyotaroyp@gmail.com>
2025-05-20 00:10:07 +0900

    [add] LayoutObject

---
+++ saba/saba_core/src/renderer/layout/layout_object.rs
@@ -0,0 +1,40 @@
+use core::cell::RefCell;
+
+use alloc::rc::{Rc, Weak};
+
+use crate::renderer::dom::node::Node;
+
+#[derive(Debug, Clone)]
+pub struct LayoutObject {
+    kind: LayoutObjectKind,
+    node: Option<Rc<RefCell<Node>>>,
+    first_child: Option<Rc<RefCell<LayoutObject>>>,
+    next_sibling: Option<Rc<RefCell<LayoutObject>>>,
+    parent: Weak<RefCell<LayoutObject>>,
+    style: ComputeStyle,
+    point: LayoutPoint,
+    size: LayoutSize,
+}
+
+impl LayoutObject {
+    pub fn new(
+        node: Option<Rc<RefCell<Node>>>,
+        parent_object: &Option<Rc<RefCell<LayoutObject>>>,
+    ) -> Self {
+        let parent = match parent_object {
+            Some(p) => Rc::downgrade(p),
+            None => Weak::new(),
+        };
+
+        Self {
+            kind: LayoutObjectKind::Block,
+            node: node.clone(),
+            first_child: None,
+            next_sibling: None,
+            parent: parent,
+            style: ComputedStyle::new(),
+            point: LayoutPoint::new(0, 0),
+            size: LayoutSize::new(0, 0),
+        }
+    }
+}
