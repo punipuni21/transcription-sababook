71c005ecaf05223094948290edd78c0d356fe735
punipuni21 <watanaberyotaroyp@gmail.com>
2025-05-29 23:49:04 +0900

    [impl] calculate_node_position

--- saba/saba_core/src/renderer/layout/layout_view.rs
+++ saba/saba_core/src/renderer/layout/layout_view.rs
@@ -61,6 +61,41 @@ impl LayoutView {
             n.borrow_mut().compute_size(parent_size);
         }
     }
+
+    fn calculate_node_position(
+        node: &Option<Rc<RefCell<LayoutObject>>>,
+        parent_point: LayoutPoint,
+        previous_sibling_kind: LayoutObjectKind,
+        previous_sibling_point: Option<LayoutPoint>,
+        previous_sibling_size: Option<LayoutSize>,
+    ) {
+        if let Some(n) = node {
+            n.borrow_mut().compute_position(
+                parent_point,
+                previous_sibling_kind,
+                previous_sibling_point,
+                previous_sibling_size,
+            );
+
+            let first_child = n.borrow().first_child();
+            Self::calculate_node_position(
+                &first_child,
+                n.borrow().point(),
+                LayoutObjectKind::Block,
+                None,
+                None,
+            );
+
+            let next_sibling = n.borrow().next_sibling();
+            Self::calculate_node_position(
+                &next_sibling,
+                parent_point,
+                n.borrow().kind(),
+                Some(n.borrow().point()),
+                Some(n.borrow().size()),
+            );
+        }
+    }
 }
 
 fn build_layout_tree(
