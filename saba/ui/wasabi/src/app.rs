63f4471d03c32d33e2f677ca6bd1b1fc0853424a
punipuni21 <watanaberyotaroyp@gmail.com>
2025-06-05 23:37:10 +0900

    [add] WasabiUI

--- saba/ui/wasabi/src/app.rs
+++ saba/ui/wasabi/src/app.rs
@@ -1,1 +1,11 @@
+use core::cell::RefCell;
 
+use alloc::rc::Rc;
+use noli::window::Window;
+use saba_core::browser::Browser;
+
+#[derive(Debug)]
+pub struct WasabiUI {
+    browser: Rc<RefCell<Browser>>,
+    window: Window,
+}
