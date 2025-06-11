530bcd37967a884b8a818454eb3099b209881d4d
punipuni21 <watanaberyotaroyp@gmail.com>
2025-06-11 23:44:51 +0900

    [add] constructor

--- saba/ui/wasabi/src/app.rs
+++ saba/ui/wasabi/src/app.rs
@@ -1,11 +1,31 @@
 use core::cell::RefCell;
 
-use alloc::rc::Rc;
+use alloc::{rc::Rc, string::ToString};
 use noli::window::Window;
-use saba_core::browser::Browser;
+use saba_core::{
+    browser::Browser,
+    constants::{WHITE, WINDOW_HEIGHT, WINDOW_INIT_X_POS, WINDOW_INIT_Y_POS},
+};
 
 #[derive(Debug)]
 pub struct WasabiUI {
     browser: Rc<RefCell<Browser>>,
     window: Window,
+}
+
+impl WasabiUI {
+    pub fn new(browser: Rc<RefCell<Browser>>) -> Self {
+        Self {
+            browser: browser,
+            window: Window::new(
+                "saba".to_string(),
+                WHITE,
+                WINDOW_INIT_X_POS,
+                WINDOW_INIT_Y_POS,
+                WINDOW_HEIGHT,
+                WINDOW_HEIGHT,
+            )
+            .unwrap(),
+        }
+    }
 }
