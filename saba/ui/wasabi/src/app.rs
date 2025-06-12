4274a5e64f85b2325a5ea9ac2c1013cdddc5200b
punipuni21 <watanaberyotaroyp@gmail.com>
2025-06-12 22:18:46 +0900

    fn setup_toolbar(&mut self) -> OsResult<()> { [impl]

--- saba/ui/wasabi/src/app.rs
+++ saba/ui/wasabi/src/app.rs
@@ -1,10 +1,14 @@
 use core::cell::RefCell;
 
 use alloc::{rc::Rc, string::ToString};
-use noli::window::Window;
+use noli::error::Result as OsResult;
+use noli::window::{StringSize, Window};
 use saba_core::{
     browser::Browser,
-    constants::{WHITE, WINDOW_HEIGHT, WINDOW_INIT_X_POS, WINDOW_INIT_Y_POS},
+    constants::{
+        ADDRESSBAR_HEIGHT, BLACK, DARKGREY, GREY, LIGHTGREY, TOOLBAR_HEIGHT, WHITE, WINDOW_HEIGHT,
+        WINDOW_INIT_X_POS, WINDOW_INIT_Y_POS, WINDOW_WIDTH,
+    },
 };
 
 #[derive(Debug)]
@@ -27,5 +31,36 @@ impl WasabiUI {
             )
             .unwrap(),
         }
+    }
+
+    fn setup_toolbar(&mut self) -> OsResult<()> {
+        self.window
+            .fill_rect(LIGHTGREY, 0, 0, WINDOW_WIDTH, TOOLBAR_HEIGHT)?;
+
+        self.window
+            .draw_line(GREY, 0, TOOLBAR_HEIGHT, WINDOW_WIDTH - 1, TOOLBAR_HEIGHT)?;
+
+        self.window.draw_line(
+            DARKGREY,
+            0,
+            TOOLBAR_HEIGHT + 1,
+            WINDOW_WIDTH - 1,
+            TOOLBAR_HEIGHT + 1,
+        )?;
+
+        self.window
+            .draw_string(BLACK, 5, 5, "Adress:", StringSize::Medium, false)?;
+
+        self.window
+            .fill_rect(WHITE, 70, 2, WINDOW_WIDTH - 74, 2 + ADDRESSBAR_HEIGHT)?;
+
+        self.window.draw_line(GREY, 70, 2, WINDOW_WIDTH - 4, 2)?;
+        self.window
+            .draw_line(GREY, 70, 2, 70, 2 + ADDRESSBAR_HEIGHT)?;
+
+        self.window.draw_line(BLACK, 71, 3, WINDOW_WIDTH - 5, 3)?;
+        self.window
+            .draw_line(GREY, 71, 3, 71, 1 + ADDRESSBAR_HEIGHT)?;
+        Ok(())
     }
 }
