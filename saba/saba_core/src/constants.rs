ae94eaeb783e550e3860b6cd66e7fdbcb327544b
punipuni21 <watanaberyotaroyp@gmail.com>
2025-06-12 22:10:25 +0900

    [add] constant

--- saba/saba_core/src/constants.rs
+++ saba/saba_core/src/constants.rs
@@ -4,6 +4,8 @@ pub static GREY: u32 = 0x808080;
 pub static DARKGREY: u32 = 0x5a5a5a;
 pub static BLACK: u32 = 0x000000;
 
+pub static ADDRESSBAR_HEIGHT: i64 = 20;
+
 pub static WINDOW_INIT_X_POS: i64 = 30;
 pub static WINDOW_INIT_Y_POS: i64 = 50;
 
